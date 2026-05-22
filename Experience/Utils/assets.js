cc8b4aa33bc98bdfa2b7da84d999d31abf9f5c04
Ramis <ramis.batrov@icloud.com>
2026-05-22 15:05:55 +0300

    2 fix: path for /room models

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -2,7 +2,7 @@ export default [
     {
         name: "room",
         type: "glbModel",
-        path: "/room/models/room_portfolio_version_3.glb"
+        path: 'models/room_portfolio_version_3.glb'
     },
     {        
         name: "screen",
